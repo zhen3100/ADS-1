0c4f598bede164d2d01a74614f26f8e221929c2c
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2022-02-07 11:48:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,41 +5,41 @@
 #include "alg.h"
 
 
-TEST(ads-1, checkPrime1) {
+TEST(ads1, checkPrime1) {
   EXPECT_EQ(true, checkPrime(2));
 }
-TEST(ads-1, checkPrime2) {
+TEST(ads1, checkPrime2) {
   EXPECT_EQ(true, checkPrime(3));
 }
-TEST(ads-1, checkPrime3) {
+TEST(ads1, checkPrime3) {
   EXPECT_EQ(false, checkPrime(12));
 }
-TEST(ads-1, nPrime1) {
+TEST(ads1, nPrime1) {
   uint64_t res = nPrime(6);
   EXPECT_EQ(13, res);
 }
-TEST(ads-1, nPrime2) {
+TEST(ads1, nPrime2) {
   uint64_t res = nPrime(500);
   EXPECT_EQ(3571, res);
 }
-TEST(ads-1, nextPrime1) {
+TEST(ads1, nextPrime1) {
   uint64_t res = nextPrime(1031);
   EXPECT_EQ(1033, res);
 }
-TEST(ads-1, nextPrime2) {
+TEST(ads1, nextPrime2) {
   uint64_t res = nextPrime(3559);
   EXPECT_EQ(3571, res);
 }
-TEST(ads-1, nextPrime3) {
+TEST(ads1, nextPrime3) {
   uint64_t res = nextPrime(2);
   EXPECT_EQ(3, res);
 }
-TEST(ads-1, sumPrime1) {
+TEST(ads1, sumPrime1) {
   uint64_t res = sumPrime(2000000);
   uint64_t expected = 142913828922;
   EXPECT_EQ(expected, res);
 }
-TEST(ads-1, sumPrime2) {
+TEST(ads1, sumPrime2) {
   uint64_t res = sumPrime(10);
   uint64_t expected = 17;
   EXPECT_EQ(expected, res);
