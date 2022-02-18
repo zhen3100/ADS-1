dee49cb6eefe34a87c862baf5e08cb2b73e5b180
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-02-18 22:43:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,50 +5,49 @@
 
 
 bool checkPrime(uint64_t value) {
-for (int i = 2; i <= sqrt(value); i++) {
-		if ((value) % i == 0) {
-			return false;
-			break;
-		}
-	}
-	return true;
+    for (int i = 2; i <= sqrt(value); i++) {
+        if ((value) % i == 0) {
+            return false;
+            break;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-int a = 1;
-	int c = 1;
-	int value = 2;
-	while (a != 0) {
-		if (checkPrime(value) == true) {
-			if (n == c) {
-				a = 0;
-				return value;
-			}
-			else {
-				c += 1;
-			}
-		}
-		value += 1;
-	}
+    int a = 1;
+    int c = 1;
+    int value = 1;
+    while (a != 0) {
+        value += 1;
+        if (checkPrime(value) == true) {
+            if (n == c) {
+                a = 0;
+            } else {
+                c += 1;
+            }
+        }
+    }
+    return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-int a = 1;
-	while (a != 0) {
-		if (checkPrime(value+1) == true) {
-			a = 0;
-			return value + 1;
-		}
-		value += 1;
-	}
+    int a = 1;
+    while (a != 0) {
+        if (checkPrime(value+1) == true) {
+            a = 0;
+        }
+        value += 1;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-int s = 0;
-	for (int i = 2; i < hbound; i++) {
-		if (checkPrime(i) == true) {
-			s += i;
-		}
-	}
-	return s;
+    int s = 0;
+    for (int i = 2; i < hbound; i++) {
+        if (checkPrime(i) == true) {
+            s += i;
+        }
+    }
+    return s;
 }
