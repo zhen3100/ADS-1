75ccadf67c5de79407e0afee451a0a3d47d46595
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-02-18 23:37:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include <math.h>
 #include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-    for (int i = 2; i <= sqrt(value); i++) {
+    for (int i = 2; i*i <= value; i++) {
         if ((value) % i == 0) {
             return false;
             break;
